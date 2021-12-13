edc3a73cc72c1101b46b6034d3319a9470ec764c
eprenatt <75020381+eprenatt@users.noreply.github.com>
2021-12-13 17:34:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+setInterval(function(){
+
+var Button = document.getElementsByClassName("typ-ad-skip-button");
+if(Button != undefined && Button.length > 0){
+    console.log("AD");
+    Button[0].click;
+}
+
+},3000)
