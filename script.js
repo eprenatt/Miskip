85b35e374d5c44aeb11ed6779e33a330465adfed
eprenatt <75020381+eprenatt@users.noreply.github.com>
2022-01-04 20:12:59 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-setInterval(function(){
-
-var Button = document.getElementsByClassName("typ-ad-skip-button");
-if(Button != undefined && Button.length > 0){
-    console.log("AD");
-    Button[0].click;
-}
-
-},3000)
